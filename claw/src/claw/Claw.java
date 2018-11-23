c24882a12894c2342ef540979f775e5cde46c7cc
Huỳnh Ngọc Thiên Trang <33373248+thientrang2106@users.noreply.github.com>
2018-11-23 14:37:41 +0700

    create

--- claw/src/claw/Claw.java
+++ claw/src/claw/Claw.java
@@ -5,6 +5,10 @@
  */
 package claw;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  *
  * @author Thien Trang
@@ -14,8 +18,11 @@ public class Claw {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        Document doc = Jsoup.connect("https://www.amazon.com/Samsung-Chromebook-Wi-Fi-11-6-Inch-Refurbished/dp/B00M9K7L8S/ref=br_asw_pdt-2?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=PE7EVYS6Z114V2CT7KQR&pf_rd_t=36701&pf_rd_p=bdec4768-ebeb-4bd3-96e7-efd23aabb29d&pf_rd_i=desktop").get();
+        String tilte = doc.title();
+        System.out.println(tilte);
     }
     
 }
