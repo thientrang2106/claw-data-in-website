059cb536fa369fa303e2237a622c30eb6dccdad6
Huỳnh Ngọc Thiên Trang <33373248+thientrang2106@users.noreply.github.com>
2019-01-09 14:52:49 +0700

    up

--- claw/src/claw/Claw.java
+++ claw/src/claw/Claw.java
@@ -5,24 +5,103 @@
  */
 package claw;
 
+import java.applet.Applet;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
+import org.jsoup.Connection;
+import org.jsoup.Connection.Method;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 /**
  *
  * @author Thien Trang
  */
-public class Claw {
+public class Claw extends Applet{
 
     /**
      * @param args the command line arguments
      */
+    static  void writeFile(String t){
+        try {
+        //Bước 1: Tạo đối tượng luồng và liên kết nguồn dữ liệu
+        FileOutputStream fos = new FileOutputStream("text/output.txt");
+        DataOutputStream dos = new DataOutputStream(fos);
+
+        //Bước 2: Ghi dữ liệu
+        dos.writeChars(t);
+
+        //Bước 3: Đóng luồng
+        fos.close();
+        dos.close();
+        System.out.println("Done!");
+       } catch (IOException ex) {
+         ex.printStackTrace();
+       } 
+    }
+    private static  Document connect(String url) throws IOException{
+        Connection.Response res = Jsoup.connect(url)
+                .method(Method.POST)
+                .userAgent("a")
+                .timeout(100 * 1000)
+                .ignoreHttpErrors(true)
+                .execute();
+        Map<String, String> cookies = res.cookies();
+        Document doc = Jsoup.connect(url)
+                .method(Method.POST)
+                .userAgent("a")
+                .timeout(100 * 1000)
+                .ignoreHttpErrors(true)
+                .cookies(cookies)
+                .get();
+        return doc;
+    }
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        Document doc = Jsoup.connect("https://www.amazon.com/Samsung-Chromebook-Wi-Fi-11-6-Inch-Refurbished/dp/B00M9K7L8S/ref=br_asw_pdt-2?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=PE7EVYS6Z114V2CT7KQR&pf_rd_t=36701&pf_rd_p=bdec4768-ebeb-4bd3-96e7-efd23aabb29d&pf_rd_i=desktop").get();
-        String tilte = doc.title();
-        System.out.println(tilte);
+        int numPage = 1;
+        String url = "https://www.amazon.com/Samsung-Chromebook-Wi-Fi-11-6-Inch-Refurbished/product-reviews/B00M9K7L8S/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews";
+        Document doc = connect(url);
+        Element com = doc.getElementsByClass("page-button").last();
+        int num = 0;
+        String title = "";
+        if(com != null){
+            title = com.toString();
+            int len = title.length() - 9;
+            title = title.substring(0, len);
+            String v = ">";
+            while(title.charAt(0) == '<'){
+                title = title.substring(title.indexOf(v) + 1);
+            }
+            num = Integer.parseInt(title);
+        }else{
+            System.out.println("null");
+        }
+        System.out.println(num);
+        
+        String data = "";
+        for(int i = 1; i <= num; i++){
+            System.out.println(i);
+            url = "https://www.amazon.com/Samsung-Chromebook-Wi-Fi-11-6-Inch-Refurbished/product-reviews/B00M9K7L8S/ref=cm_cr_arp_d_paging_btm_+"+i+"?ie=UTF8&reviewerType=all_reviews&pageNumber="+i;
+            Document doc1 = connect(url);
+            //Element com1 = doc1.getElementsByClass("a-section a-spacing-none reviews-content a-size-base").first();
+            Elements com1 = doc1.select("span[data-hook=\"review-body\"]");
+            if(com1 != null){
+                String te = ">";
+                for(int j = 0; j < com1.size(); j++){
+                    String c = com1.get(j).toString();
+                    data += c.substring(c.indexOf(te) + 1, c.length() - 7) + "\n";
+                }
+                
+            }else{
+                System.out.println("null");
+            }
+        }
+        
+        writeFile(data);
     }
     
 }
